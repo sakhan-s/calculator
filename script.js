cd7452803c63bc942800af7b3d731d17be47788d
sakhan-s <siradzhov@ya.ru>
2020-09-26 21:14:59 +0300

    feat: change calc layout

--- script.js
+++ script.js
@@ -1,38 +1,94 @@
-let num1 = document.getElementsByClassName('prev-number');
-let num2 = document.getElementsByClassName('cur-number');;
-let out = document.getElementsByClassName('output');
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operator');
+const clearBtns = document.querySelectorAll('.clear-btn');
+const decimalBtn = document.getElementById('decimal');
+const result = document.getElementById('result');
+const display = document.getElementById('display');
+let MemoryCurrentNumber = 0;
+let MemoryNewNumber = false;
+let MemoryPendingOperation = '';
 
-function plus() {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1)
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2)
-
-    out.innerHTML = num1 + num2;
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    number.addEventListener('click', function(e) {
+        numberPress(e.target.textContent);
+    });
 }
 
-function minus() {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1)
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2)
-
-    out.innerHTML = num1 - num2;
+for (var i = 0; i < operations.length; i++) {
+    var operationBtn = operations[i];
+    operationBtn.addEventListener('click', function(e) {
+        operationPress(e.target.textContent);
+    });
 }
 
-function multiplication() {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1)
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2)
-    out.innerHTML = num1 * num2;
+for (var i = 0; i < clearBtns.length; i++) {
+    var clearBtn = clearBtns[i];
+    clearBtn.addEventListener('click', function(e) {
+        clear(e.target.textContent);
+    });
 }
 
-function division() {
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1)
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2)
+decimalBtn.addEventListener('click', decimal);
 
-    out.innerHTML = num1 / num2;
+function numberPress(number) {
+    if (MemoryNewNumber) {
+        display.value = number;
+        MemoryNewNumber = false;
+    } else {
+        if (display.value === '0') {
+            display.value = number;
+        } else {
+            display.value += number;
+        }
+    }
+}
+
+function operationPress(op) {
+    let localOperationMemory = display.value;
+
+    if (MemoryNewNumber && MemoryPendingOperation !== '=') {
+        display.value = MemoryCurrentNumber;
+    } else {
+        MemoryNewNumber = true;
+        if (MemoryPendingOperation === '+') {
+            MemoryCurrentNumber += +localOperationMemory;
+        } else if (MemoryPendingOperation === '-') {
+            MemoryCurrentNumber -= +localOperationMemory;
+        } else if (MemoryPendingOperation === '*') {
+            MemoryCurrentNumber *= +localOperationMemory;
+        } else if (MemoryPendingOperation === '/') {
+            MemoryCurrentNumber /= +localOperationMemory;
+        } else {
+            MemoryCurrentNumber = +localOperationMemory;
+        }
+        display.value = MemoryCurrentNumber;
+        MemoryPendingOperation = op;
+    }
+}
+
+function decimal(argument) {
+    let localDecimalMemory = display.value;
+
+    if (MemoryNewNumber) {
+        localDecimalMemory = '0.';
+        MemoryNewNumber = false;
+    } else {
+        if (localDecimalMemory.indexOf('.') === -1) {
+            localDecimalMemory += '.';
+        }
+    }
+    display.value = localDecimalMemory;
+}
+
+function clear(id) {
+    if (id === 'clear') {
+        display.value = '0';
+        MemoryNewNumber = true;
+    } else if (id === 'del') {
+        display.value = '0';
+        MemoryNewNumber = true;
+        MemoryCurrentNumber = 0;
+        MemoryPendingOperation = '';
+    }
 }
